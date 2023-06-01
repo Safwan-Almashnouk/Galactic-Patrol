30432f4148c2bea7a72a353099c1a9b335972877
Eliza Voets <25413@ma-web.nl>
2023-06-01 09:57:27 +0200

    stole safo's assets <3

---
+++ GameSpace/Assets/Scripts/spawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class spawner : MonoBehaviour
+
+{
+     public GameObject enemy;
+
+     public float enemyInterval;
+
+    public Transform point;
+
+    void Start()
+    {
+        StartCoroutine(spawnEnemy(enemyInterval, enemy));
+        point = GameObject.Find("MeteorSpawner").GetComponent<Transform>();
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        Vector3 spawnPoint = new Vector3(point.position.x , point.position.y, point.position.z + Random.Range(7, -7));
+        GameObject newEnemy = Instantiate(enemy, spawnPoint, Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
+
+    }
+}
