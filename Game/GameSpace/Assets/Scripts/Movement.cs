0d9c288d37c4269e8c6d799cf470b7f8a00cb99c
Safwan <35345@ma-web.nl>
2023-06-08 13:06:25 +0200

    Movment completed

--- Game/GameSpace/Assets/Scripts/Movement.cs
+++ Game/GameSpace/Assets/Scripts/Movement.cs
@@ -8,6 +8,15 @@ public class Movement : MonoBehaviour
     
    
     private Rigidbody obj;
+    public GameObject camera; 
+    public float rotate = 250f;
+    public float speed= 10;
+    Vector3 cameraPos;
+    public float endDes = 7;
+    public float endDesFr = 7;
+    public float endDesBk = 3;
+    public float RotPos = 35;
+    public float RotSpeed = 10;
 
 
     private void Start()
@@ -16,54 +25,80 @@ public class Movement : MonoBehaviour
     }
     public void Update()
     {
+        Vector3 newPosition = transform.localPosition;
+        obj.velocity= new Vector3 (-5, 0, 0 );
+        cameraPos = new Vector3(obj.velocity.x * Time.deltaTime + camera.transform.position.x, camera.transform.position.y, camera.transform.position.z);
+        camera.transform.position = cameraPos;
 
-       obj.velocity= new Vector3 (-5, 0, 0 );
-        
+        Vector3 slashSpeed = Vector3.zero;
+        Vector3 rotation = new Vector3(0,transform.localEulerAngles.y, transform.localEulerAngles.z);
+
         if (Input.GetKey(KeyCode.A)) 
-        { 
-            transform.position += new Vector3(0, 0, -10 ) * Time.deltaTime;
+        {
+            slashSpeed.z -= speed;
+           rotation.z += RotSpeed * Time.deltaTime;
+
         }
         if (Input.GetKey(KeyCode.D)) 
         {
-            transform.position += new Vector3(0, 0, 10 ) * Time.deltaTime;
+            slashSpeed.z += speed;
+           rotation.z += -RotSpeed * Time.deltaTime;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += new Vector3(10,0,0) * Time.deltaTime;
+            slashSpeed.x += speed;
         }
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(-10, 0, 0) * Time.deltaTime;
+            slashSpeed.x -= speed;
+        }
+        
+        
+        //Rotation 
+        if (rotation.z > RotPos && rotation.z < 180)
+         {
+            
+            rotation.z = RotPos;
+             
+         }
+         if(rotation.z < 360-RotPos && rotation.z > 180)
+         {
+             rotation.z = -RotPos;
+             
+         }
+
+        
+        transform.localEulerAngles = rotation;
+        
+        //Movmenet 
+        slashSpeed.Normalize();
+        slashSpeed *= speed * Time.deltaTime;
+        newPosition += slashSpeed;
+
+
+        if (newPosition.x <= cameraPos.x - endDesBk)
+        {
+            newPosition.x = cameraPos.x - endDesBk;
+        }
+        if (newPosition.x >= cameraPos.x + endDesFr)
+        {
+            newPosition.x = (cameraPos.x + endDesFr);
+        }
+        if (newPosition.z <= -endDes)
+        {
+            newPosition.z = -endDes;
+        }
+        if (newPosition.z >= endDes)
+        {
+            newPosition.z = endDes;
         }
 
-        movment();
-
+        transform.localPosition = newPosition;
     }
 
     public void movment()
     {
+        //Vector3 newPosition = transform.position;
        
-        
-            if (transform.position.z >= -7 )
-            {
-
-            }
-            else
-            {
-                Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, -7f);
-                transform.position = newPosition;
-            }
-
-            if (transform.position.z <= 7)
-            {
-
-            }
-            else
-            {
-                Vector3 newPosition = new Vector3(transform.position.x, transform.position.y, 7);
-                transform.position = newPosition;
-            }
-
-
     }
 }
