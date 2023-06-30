9beebb1e994bc7a045d516a46e0df22cd105db1c
Safwan-Almashnouk <114736059+Safwan-Almashnouk@users.noreply.github.com>
2023-06-30 11:55:21 +0200

    fixed

--- Game/GameSpace/Assets/BossAttack.cs
+++ Game/GameSpace/Assets/BossAttack.cs
@@ -1,48 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-
-using System.Drawing;
-using Unity.VisualScripting;
-using UnityEngine;
-
-public class BossAttack : MonoBehaviour
-{
-    public GameObject ray;
-    public GameObject dangerSign;
-    public float rayInternval;
-    public Transform point;
-    public float location;
-    public float delay;
-
-    void Start()
-    {
-        StartCoroutine(spawnRay(rayInternval, ray, dangerSign));
-        point = GameObject.Find("Boss").GetComponent<Transform>();
-
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 Pos = transform.position;
-    }
-
-    private IEnumerator spawnRay(float interval, GameObject ray,GameObject dangerSign)
-    {
-        yield return new WaitForSeconds(interval);
-        Vector3 spawnPoint = new Vector3(point.position.x , point.position.y, point.position.z + Random.Range(7, -7));
-        Vector3 location = dangerSign.transform.position = new Vector3(spawnPoint.x + 30, spawnPoint.y + 0, spawnPoint.z + 0);
-        Quaternion rotation = Quaternion.Euler(point.rotation.x, point.rotation.y - 90, point.rotation.z);
-        GameObject newRay = Instantiate(ray, spawnPoint, rotation);
-       // GameObject newSign = Instantiate(dangerSign, spawnPoint, location, rotation);
-        StartCoroutine(spawnRay(interval, ray, dangerSign));
-
-    }
-
-
-}
-
 using UnityEngine;
 using UnityEngine.UIElements;
 using System.Threading.Tasks;
@@ -103,5 +60,4 @@ public class BossAttack : MonoBehaviour
 
     }
 }
-
 
