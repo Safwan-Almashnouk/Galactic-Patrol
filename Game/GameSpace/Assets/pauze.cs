6c7fe12fd3a064db0a6e5cab1b3ea42d450b5f91
Safwan-Almashnouk <114736059+Safwan-Almashnouk@users.noreply.github.com>
2023-07-05 21:14:03 +0200

    fixingg

--- Game/GameSpace/Assets/pauze.cs
+++ Game/GameSpace/Assets/pauze.cs
@@ -5,10 +5,42 @@ using UnityEngine.UI;
 
 public class pauze : MonoBehaviour
 {
-    public GameObject gameUI;
-    public void AntiMove()
-    { 
-       gameUI.SetActive(false);
-        Time.timeScale = 1;
+    public GameObject PauzeMenu;
+
+    public bool isPaused;
+
+    private void Start()
+    {
+        PauzeMenu.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isPaused)
+            {
+                ResumeGame();
+            }
+            else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void PauseGame()
+    {
+        PauzeMenu.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+
+    public void ResumeGame()
+    {
+        PauzeMenu.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
     }
 }
+
